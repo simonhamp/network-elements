df7313cbf19ad2233bb2e35241866b9a5400155d
Simon Hamp <simon.hamp@me.com>
2017-11-13 18:14:25 +0000

    Initial commit
    Which isn't much use yet

---
+++ src/Models/Leader.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Leader extends Model
+{
+    // We'll need to respond to pings and get updates
+
+    protected function getUpdates()
+    {
+        // Fetch the updates
+    }
+}
