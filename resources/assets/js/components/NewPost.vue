a250d16b8b215c97c2ae9fff7f4dc8c96f10903d
Simon Hamp <simon@flipstorm.co.uk>
2018-02-10 14:16:11 +0000

    Add New Post field component

---
+++ resources/assets/js/components/NewPost.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h3>New Post</h3>
+
+        <div class="form-group">
+            <textarea class="form-control"></textarea>
+        </div>
+
+        <button class="btn btn-primary">Post</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
