dd1187268d9582ed2443f0ac95cb912a468049da
Simon Hamp <simon.hamp@me.com>
2019-05-11 17:40:54 +0100

    Websockets prep

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -43,13 +43,16 @@ if (token) {
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo'
+import Echo from "laravel-echo"
 
-// window.Pusher = require('pusher-js');
+window.Pusher = require('pusher-js');
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     encrypted: true
-// });
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: 'e9591a87f1ab4a462637',
+    cluster: 'eu',
+    wsHost: window.location.hostname,
+    wsPort: 6001,
+    disableStats: true,
+    encrypted: false,
+});
